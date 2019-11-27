02a85faed1ea4fea55688e7516d734c9b3bc579d
rogcomfox <ygladiensyah@gmail.com>
2019-11-28 03:36:22 +0700

    fix sorting task

--- src/com/nusantarian/tugas/MergeSort.java
+++ src/com/nusantarian/tugas/MergeSort.java
@@ -1,5 +1,56 @@
 package com.nusantarian.tugas;
 
-public class MergeSort {
+import java.util.Arrays;
 
+public class MergeSort {
+    private static void mergeSort(int[] arr, int left, int right) {
+        if (right <= left) return;
+
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        //membuat array sementara untuk array kiri dan kanan
+        int[] left_arr = new int[mid - left + 1];
+        int[] right_arr = new int[right - mid];
+        //mengisi pada array sementara
+        if (left_arr.length >= 0) System.arraycopy(arr, left, left_arr, 0, left_arr.length);
+        for (int i = 0; i < right_arr.length; i++) {
+            right_arr[i] = arr[mid + i + 1];
+        }
+        int left_index = 0;
+        int right_index = 0;
+
+        for (int i = left; i < right + 1; i++) {
+            if (left_index < left_arr.length && right_index < right_arr.length) {
+                if (left_arr[left_index] < right_arr[right_index]) {
+                    arr[i] = left_arr[left_index];
+                    left_index++;
+                } else {
+                    arr[i] = right_arr[right_index];
+                    right_index++;
+                }
+            }
+            else if (left_index < left_arr.length){
+                //ketika elemen dari array kanan telah terkopi, kopi sisa index array kiri
+                arr[i] = left_arr[left_index];
+                left_index++;
+            }
+            else if (right_index < right_arr.length){
+                //ketika elemen dari array kiri telah terkopi, kopi sisa indeks array kanan
+                arr[i] = right_arr[right_index];
+                right_index++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] sample_arr = {6, 8, 4, 9, 5, 3, 7, 8, 1};
+        System.out.println("Array Sebelum Di Sorting\n" + Arrays.toString(sample_arr));
+        mergeSort(sample_arr, 0, sample_arr.length - 1);
+        System.out.println("Array Sesudah Di Sorting\n" + Arrays.toString(sample_arr));
+    }
 }
